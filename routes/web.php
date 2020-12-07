d191f718b4bea445297bfcd8aabbf57231338c03
nafarahisya <nfarahisya@gmail.com>
2020-12-08 01:00:06 +0700

    cek server

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('{name}', function ($name) {
-    if (file_exists(public_path($name))) {
-        return redirect(url('public/' . $name));
-    } else {
-        abort(404);
-    }
-})->where('name', '(.*)');
+// Route::get('{name}', function ($name) {
+//     if (file_exists(public_path($name))) {
+//         return redirect(url('public/' . $name));
+//     } else {
+//         abort(404);
+//     }
+// })->where('name', '(.*)');
